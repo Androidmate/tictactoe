48635565b08df10bd0e3c6b48d4d741eab978da2
Peeyush Kumar <24976633+PeeyushKumar@users.noreply.github.com>
2021-10-06 17:14:08 +0530

    created basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+
+
+const App = () => {
+
+  const [grid, setGrid] = useState([]);
+
+  useEffect(() => {
+    
+    const newGrid = []
+    for (let i=0; i<9; i++) {
+      newGrid.push('');
+    }
+
+    setGrid(newGrid);
+  }, [])
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        grid.map(ch => <div className='box'>{ch}</div>)
+      }
     </div>
   );
 }
